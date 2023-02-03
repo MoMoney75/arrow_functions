74a5d4945591cfcd477e32325440732b7130c1a7
Mauricio <maudusi12@gmail.com>
2023-02-02 22:29:02 -0500

    answers for arrow functions exercise for SPRINGBOARD

---
+++ app.js
@@ -0,0 +1,27 @@
+//1
+let arr = [1, 2, 3];
+function double(arr) {
+  return arr.map(function (val) {
+    return val * 2;
+  });
+}
+
+//IS THE SAME AS:
+
+const double = (arr) => arr.map((val) => val * 2);
+
+//2
+function squareAndFindEvens(numbers) {
+  var squares = numbers.map(function (num) {
+    return num ** 2;
+  });
+  var evens = squares.filter(function (square) {
+    return square % 2 === 0;
+  });
+  return evens;
+}
+
+// IS THE SAME AS :
+
+const squareAndFindEvens = (numbers) =>
+  numbers.map((num) => num ** 2).filter((square) => square % 2 === 0);
